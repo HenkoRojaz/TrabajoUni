7c5d0d1341d4fca64e529bf3e0213554b7e8c2f8
HenkoRojaz <134526246+HenkoRojaz@users.noreply.github.com>
2026-03-27 10:02:58 -0600

    Update molde_servidor1.py
    Se agrega en L1 "import os" con el fin de tener comunicacion con el ordenador
    Se crea el metodo "validar_estado con el fin de verificar si las ip estan o no activas

--- molde_servidor1.py
+++ molde_servidor1.py
@@ -1,3 +1,5 @@
+import os
+
 class Servidor:
     def __init__(self, ip, hostname, os):
         self.ip = ip
@@ -5,11 +7,24 @@ class Servidor:
         self.os = os
         self.estado = "Desconocido"
     
+    def verificar_estado(self):
+        respuesta = os.system(f"ping -n 1 {self.ip}")
+
+        if respuesta == 0:
+            self.estado = "Activo"
+        
+        else:
+            self.estado = "Caido"
+            
     def mostrar_info(self):
         print(f"[{self.estado}] {self.hostname} ({self.ip}) - SO: {self.os}")
 
 server_web = Servidor("192.168.1.0","WEB-PROD-01","LINUX")
 server_db = Servidor("10.0.0.5","DB-MAIN","Windows Server")
 
+server_db.verificar_estado()
+server_web.verificar_estado()
+
 server_db.mostrar_info()
 server_web.mostrar_info()
+
